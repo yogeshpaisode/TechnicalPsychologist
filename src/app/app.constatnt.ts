0a1dd4c69860ecbae6299a70527608fe78371261
Yogesh D.Paisode <yogesh.paisode@gmail.com>
2019-02-15 16:15:15 +0530

    Fix for data.

---
+++ src/app/app.constatnt.ts
@@ -0,0 +1,7 @@
+
+export const dsylexia: string = 'dsylexia';
+export const readingWrinting: string = 'Reading & Speaking';
+export const text: string = 'Text';
+export const audio: string = 'Audio';
+export const image: string = 'Image';
+export const video: string = 'Video';
--- src/app/test/test-data.service.ts
+++ src/app/test/test-data.service.ts
@@ -1,6 +1,48 @@
 import {Injectable} from '@angular/core';
+import {audio, dsylexia, image, readingWrinting, text} from '../app.constatnt';
 
 @Injectable()
 export default class TestDataService {
 
+  public testData: any =
+    {
+      'kg': [
+        {
+          'disabilityType': dsylexia,
+          'questionType': text,
+          'isLocal': true,
+          'source': 'What is the word before "H"',
+          'answer': '',
+          'weightage': 0,
+          'learningType': readingWrinting
+        },
+        {
+          'disabilityType': dsylexia,
+          'questionType': audio,
+          'isLocal': false,
+          'source': 'pathOfAudio',
+          'answer': '',
+          'weightage': 0,
+          'learningType': 'Listening'
+        },
+        {
+          'disabilityType': dsylexia,
+          'questionType': image,
+          'isLocal': false,
+          'source': 'pathOfImage/Video',
+          'answer': '',
+          'weightage': 0,
+          'learningType': 'Recognition'
+        }
+      ],
+      'Primary': [
+
+      ],
+      'MiddleSchool': [
+
+      ],
+      'Secondary': [
+      ]
+    }
+  
 }
