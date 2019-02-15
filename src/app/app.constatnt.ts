3594267e0a2ddda7b0e84e3c39c21a2f50330c43
Vrishabh Misha <vrishabh.mishra@globallogic.com>
2019-02-15 18:13:08 +0530

    JSON updated

--- src/app/app.constatnt.ts
+++ src/app/app.constatnt.ts
@@ -1,9 +1,12 @@
 
-export const dsylexia: string = 'dsylexia';
-export const readingWrinting: string = 'Reading & Speaking';
+export const dyslexia: string = 'dyslexia';
+export const dyscalculia: string = 'dyscalculia';
+export const writing: string = 'Writing';
+export const reading: string = 'Reading';
+export const speaking: string = 'Speaking';
+export const listening: string = 'Listening';
+export const recognition: string = 'Recognition';
 export const text: string = 'Text';
 export const audio: string = 'Audio';
 export const image: string = 'Image';
 export const video: string = 'Video';
-export const listening: string = 'Listening';
-export const recognition: string = 'Recognition';
--- src/app/test/test-data.service.ts
+++ src/app/test/test-data.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {audio, dsylexia, image, listening, readingWrinting, recognition, text} from '../app.constatnt';
+import {audio, dyslexia, image, listening, reading, writing, recognition, text, speaking, dyscalculia} from '../app.constatnt';
 
 @Injectable()
 export default class TestDataService {
@@ -8,49 +8,149 @@ export default class TestDataService {
     {
       'kg': [
         {
-          'disabilityType': dsylexia,
+          'disabilityType': dyslexia,
           'questionType': text,
-          'isLocal': true,
-          'source': '1',
+          'question': 'Write your name.',
+          'isLocal': false,
+          'source': '',
           'answer': '',
           'weightage': 0,
-          'learningType': readingWrinting
+          'learningType': writing,
         },
         {
-          'disabilityType': dsylexia,
-          'questionType': text,
+          'disabilityType': dyslexia,
+          'questionType': image,
+          'question': 'Identify the geometric shape',
           'isLocal': true,
-          'source': '2',
+          'source': '../../assets/images/kindergarden_hexagon.png',
           'answer': '',
           'weightage': 0,
-          'learningType': readingWrinting
+          'learningType': recognition
         },
         {
-          'disabilityType': dsylexia,
+          'disabilityType': dyslexia,
           'questionType': text,
-          'isLocal': true,
-          'source': '3',
+          'question': 'Pronounce: Teddy Bear',
+          'isLocal': false,
+          'source': '',
           'answer': '',
           'weightage': 0,
-          'learningType': readingWrinting
+          'learningType': speaking
         },
         {
-          'disabilityType': dsylexia,
+          'disabilityType': dyslexia,
           'questionType': text,
+          'question': 'Spell: circle, cat, dog, etc.',
           'isLocal': true,
           'source': '4',
           'answer': '',
           'weightage': 0,
-          'learningType': readingWrinting
+          'learningType': speaking
         },
         {
-          'disabilityType': dsylexia,
+          'disabilityType': dyslexia,
           'questionType': text,
-          'isLocal': true,
-          'source': '5',
+          'question': 'Tell 7 days of a week',
+          'isLocal': false,
+          'source': '',
           'answer': '',
           'weightage': 0,
-          'learningType': readingWrinting
+          'learningType': speaking
+        },
+        {
+          'disabilityType': dyslexia,
+          'questionType': text,
+          'question': 'Aphabet after G? Ans: H',
+          'isLocal': false,
+          'source': '',
+          'answer': '',
+          'weightage': 0,
+          'learningType': speaking
+        },
+        {
+          'disabilityType': dyslexia,
+          'questionType': text,
+          'question': 'Confuses letters that look similar (b, d, p, q) and letters that have similar sounds (d/t; b/p; f/v)',
+          'isLocal': false,
+          'source': '',
+          'answer': '',
+          'weightage': 0,
+          'learningType': writing
+        },
+        {
+          'disabilityType': dyslexia,
+          'questionType': text,
+          'question': 'Tell a poem, or story (See the sequence)',
+          'isLocal': false,
+          'source': '',
+          'answer': '',
+          'weightage': 0
+        },
+        {
+          'disabilityType': dyslexia,
+          'questionType': text,
+          'question': 'Has trouble learning a new word, even after you’ve tried to teach it to him/her many times.',
+          'isLocal': false,
+          'source': '',
+          'answer': '',
+          'weightage': 0,
+          'learningType': speaking
+        },
+        {
+          'disabilityType': dyslexia,
+          'questionType': text,
+          'question': 'May have difficulty with rhyming. e.g. cat and rat, dog and fog, etc.',
+          'isLocal': false,
+          'source': '',
+          'answer': '',
+          'weightage': 0,
+          'learningType': speaking
+        },
+        {
+          'disabilityType': dyscalculia,
+          'questionType': text,
+          'question': '2 + 7 = ?',
+          'isLocal': false,
+          'source': '',
+          'answer': '',
+          'weightage': 0
+        },
+        {
+          'disabilityType': dyscalculia,
+          'questionType': text,
+          'question': 'Table of 2?',
+          'isLocal': false,
+          'source': '',
+          'answer': '',
+          'weightage': 0
+        },
+        {
+          'disabilityType': dyscalculia,
+          'questionType': text,
+          'question': '9 is bigger or 7?',
+          'isLocal': false,
+          'source': '',
+          'answer': '',
+          'weightage': 0
+        },
+        {
+          'disabilityType': dyscalculia,
+          'questionType': text,
+          'question': '= sign is called?',
+          'isLocal': false,
+          'source': '',
+          'answer': '',
+          'weightage': 0
+        },
+        {
+          'disabilityType': dyscalculia,
+          'questionType': text,
+          'question': '8 - 5 = ?',
+          'isLocal': false,
+          'source': '',
+          'answer': '',
+          'weightage': 0,
+          'learningType': speaking
         }
       ],
       'Primary': [
